e5edd2b3bf69c386443c73eae18075c2a8fe8ddf
annieha11 <michela.candotti@gmail.com>
2017-09-18 22:13:34 +0200

    Update my.js

--- u9/js/my.js
+++ u9/js/my.js
@@ -1,8 +1,8 @@
 $(window).scroll(function() {
 	var scroll = $(window).scrollTop();
 	if (scroll >= 500) {
-	$('header').addClass('.fixed');
+	$('header').addClass('fixed');
 	} else {
-	$('header').removeClass('.fixed');
+	$('header').removeClass('fixed');
 	}
  });
